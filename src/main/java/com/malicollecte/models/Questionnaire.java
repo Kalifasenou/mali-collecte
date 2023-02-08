7649deb5c4e82aa7f44a23c640f7dcee05657388
Kalifa SENOU <kalifasenou10@gmail.com>
2023-02-08 10:33:16 +0000

    Creations des classes entités

---
+++ src/main/java/com/malicollecte/models/Questionnaire.java
@@ -0,0 +1,37 @@
+package com.malicollecte.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Questionnaire {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    private String libellequestionnaire;
+
+    private String questionnairetype;
+
+    @OneToOne
+    private Enquete enquete;
+
+    @OneToMany
+    @JsonIgnore
+    private List<Question> questions;
+
+    private String etatAvancement;
+
+    @ManyToMany()
+    private List<User> users;
+
+
+}
